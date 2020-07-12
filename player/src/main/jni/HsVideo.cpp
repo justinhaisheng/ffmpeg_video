a53763e80339239d63bf4280bd1dd0467c7d9933
qiaoStr <236685708@qq.com>
2020-07-12 11:43:20 +0800

    video package 解码

--- player/src/main/jni/HsVideo.cpp
+++ player/src/main/jni/HsVideo.cpp
@@ -19,6 +19,12 @@ HsVideo::~HsVideo() {
 void* playVideo(void* data){
     HsVideo* video = static_cast<HsVideo *>(data);
     while (video->playstatus&&!video->playstatus->exit){
+
+        if(video->playstatus->seek){
+            av_usleep(1000*100);
+            continue;
+        }
+
         if (video->queue->getQueueSize() == 0){//没有数据
             if (!video->playstatus->load){
                 video->playstatus->load = true;
@@ -27,14 +33,56 @@ void* playVideo(void* data){
             av_usleep(1000*100);
             continue;
         }
-        AVPacket* avPacket = av_packet_alloc();
-        if (video->queue->getPacket(avPacket) == 0){
-            //解码渲染
-            LOGE("video线程中获取视频AVpacket");
+
+        if(video->playstatus->load)
+        {
+            video->playstatus->load = false;
+            video->calljava->onCallLoading(video->playstatus->load, CHILD_THREAD);
         }
+
+        AVPacket* avPacket = av_packet_alloc();
+        if (video->queue->getPacket(avPacket) != 0){
+            av_packet_free(&avPacket);
+            av_free(avPacket);
+            avPacket = NULL;
+            av_usleep(1000*100);
+            continue;
+        }
+
+        //解码
+        if(avcodec_send_packet(video->avCodecContext,avPacket)!=0){
+            av_packet_free(&avPacket);
+            av_free(avPacket);
+            avPacket = NULL;
+            av_usleep(1000*100);
+            continue;
+        }
+        AVFrame* avFrame = av_frame_alloc();
+        if (avcodec_receive_frame(video->avCodecContext,avFrame)!=0){
+            av_packet_free(&avPacket);
+            av_free(avPacket);
+            avPacket = NULL;
+
+
+            av_frame_free(&avFrame);
+            av_free(avFrame);
+            avFrame = NULL;
+
+            av_usleep(1000*100);
+            continue;
+        }
+        LOGE("子线程解码一个video  AVframe成功");
+
+
+
         av_packet_free(&avPacket);
         av_free(avPacket);
         avPacket = NULL;
+
+
+        av_frame_free(&avFrame);
+        av_free(avFrame);
+        avFrame = NULL;
     }
     pthread_exit(&video->thread_play);
 }
